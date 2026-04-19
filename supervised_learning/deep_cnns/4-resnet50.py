889d82f0f82bfef79693116ad30d21886a2a83ee
justpr09rammer <nihadqocayev152@gmail.com>
2026-04-19 11:26:44 +0000

    task 4 corrected

--- supervised_learning/deep_cnns/4-resnet50.py
+++ supervised_learning/deep_cnns/4-resnet50.py
@@ -4,62 +4,57 @@ ResNet-50 implementation
 """
 
 from tensorflow import keras as K
+
 identity_block = __import__('2-identity_block').identity_block
 projection_block = __import__('3-projection_block').projection_block
 
 
 def resnet50():
     """
-    Builds ResNet-50 architecture
+    Create the ResNet50 Model
+    :return: The ResNet50 model
     """
+    init = K.initializers.he_normal()
+    input = K.Input(shape=(224, 224, 3))
 
-    he_init = K.initializers.he_normal(seed=0)
+    conv_1 = K.layers.Conv2D(filters=64,
+                             kernel_size=(7, 7),
+                             strides=(2, 2),
+                             padding="same",
+                             kernel_initializer=init)(input)
+    norm_1 = K.layers.BatchNormalization(axis=3)(conv_1)
+    act_1 = K.layers.ReLU()(norm_1)
 
-    X = K.Input(shape=(224, 224, 3))
+    max_pool_1 = K.layers.MaxPooling2D(pool_size=(3, 3),
+                                       strides=(2, 2),
+                                       padding="same")(act_1)
 
-    x = K.layers.Conv2D(
-        64, (7, 7), strides=(2, 2),
-        padding='same',
-        kernel_initializer=he_init
-    )(X)
+    pr_block_1_2x = projection_block(max_pool_1, [64, 64, 256], s=1)
+    id_block_1_2x = identity_block(pr_block_1_2x, [64, 64, 256])
+    id_block_2_2x = identity_block(id_block_1_2x, [64, 64, 256])
 
-    x = K.layers.BatchNormalization(axis=3)(x)
-    x = K.layers.Activation('relu')(x)
-    x = K.layers.MaxPooling2D(
-        pool_size=(3, 3),
-        strides=(2, 2),
-        padding='same'
-    )(x)
+    pr_block_1_3x = projection_block(id_block_2_2x, [128, 128, 512])
+    id_block_1_3x = identity_block(pr_block_1_3x, [128, 128, 512])
+    id_block_2_3x = identity_block(id_block_1_3x, [128, 128, 512])
+    id_block_3_3x = identity_block(id_block_2_3x, [128, 128, 512])
 
-    x = projection_block(x, [64, 64, 256], s=1)
-    x = identity_block(x, [64, 64, 256])
-    x = identity_block(x, [64, 64, 256])
+    pr_block_1_4x = projection_block(id_block_3_3x, [256, 256, 1024])
+    id_block_1_4x = identity_block(pr_block_1_4x, [256, 256, 1024])
+    id_block_2_4x = identity_block(id_block_1_4x, [256, 256, 1024])
+    id_block_3_4x = identity_block(id_block_2_4x, [256, 256, 1024])
+    id_block_4_4x = identity_block(id_block_3_4x, [256, 256, 1024])
+    id_block_5_4x = identity_block(id_block_4_4x, [256, 256, 1024])
 
-    x = projection_block(x, [128, 128, 512], s=2)
-    x = identity_block(x, [128, 128, 512])
-    x = identity_block(x, [128, 128, 512])
-    x = identity_block(x, [128, 128, 512])
+    pr_block_1_5x = projection_block(id_block_5_4x, [512, 512, 2048])
+    id_block_1_5x = identity_block(pr_block_1_5x, [512, 512, 2048])
+    id_block_2_5x = identity_block(id_block_1_5x, [512, 512, 2048])
 
-    x = projection_block(x, [256, 256, 1024], s=2)
-    x = identity_block(x, [256, 256, 1024])
-    x = identity_block(x, [256, 256, 1024])
-    x = identity_block(x, [256, 256, 1024])
-    x = identity_block(x, [256, 256, 1024])
-    x = identity_block(x, [256, 256, 1024])
+    max_pool = K.layers.AvgPool2D(pool_size=(7, 7),
+                                  strides=(1, 1),
+                                  padding="valid")(id_block_2_5x)
 
-    x = projection_block(x, [512, 512, 2048], s=2)
-    x = identity_block(x, [512, 512, 2048])
-    x = identity_block(x, [512, 512, 2048])
+    dense_output = K.layers.Dense(units=1000,
+                                  activation="softmax",
+                                  kernel_initializer=init)(max_pool)
 
-    x = K.layers.AveragePooling2D(
-        pool_size=(7, 7),
-        padding='same'
-    )(x)
-
-    x = K.layers.Dense(
-        1000,
-        activation='softmax',
-        kernel_initializer=he_init
-    )(x)
-
-    return K.Model(inputs=X, outputs=x)
+    return K.models.Model(inputs=input, outputs=dense_output)
